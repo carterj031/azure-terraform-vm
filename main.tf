853caee27826a4238ff08ecd8d111d5d8eb73832
Johnny Carter <carterjohnnyprogram@gmail.com>
2026-07-10 16:09:50 -0400

    Add Azure subnet

--- main.tf
+++ main.tf
@@ -1,11 +1,6 @@
-resource "azurerm_resource_group" "main" {
-  name     = "rg-terraform-lab"
-  location = "Central US"
-}
-
-resource "azurerm_virtual_network" "main" {
-  name                = "vnet-terraform-lab"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+resource "azurerm_subnet" "main" {
+  name                 = "subnet-terraform-lab"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
